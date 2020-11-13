13f2e990058ab32f9e732bc03a469b124ce16c5e
tylersiskar <36086162+tylersiskar@users.noreply.github.com>
2020-11-13 14:43:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 
 const routing = (
