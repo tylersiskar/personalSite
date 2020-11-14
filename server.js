16f34a45a73e43e933814b3d769609e5269cfcee
tylersiskar <36086162+tylersiskar@users.noreply.github.com>
2020-11-14 14:11:53 -0500

    this could be it

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 5000;
-
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
 
-// 
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
